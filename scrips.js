2fd73fdd059d6866e9d42d39560d67d97ad7e7dd
Igor Dovganyuk <thelscon@gmail.com>
2020-06-22 21:07:13 +0300

    two

--- scrips.js
+++ scrips.js
@@ -1,1 +1,5 @@
-console.log ("hello");
+let a = Math.floor(Math.random() * 10);
+let b = prompt ('Enter you Number: ')
+console.log('Random Number - ' + a);
+console.log('You Number - ' + b)
+console.log('Result - ' + (a==b));
