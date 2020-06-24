b648b2ca6acd63fd8f451099ad6539883a9af428
Igor Dovganyuk <thelscon@gmail.com>
2020-06-24 23:04:11 +0300

    final

--- scrips.js
+++ scrips.js
@@ -1,5 +1,5 @@
 let a = Math.floor(Math.random() * 10);
-let b = prompt ('Enter you Number: ')
+let b = Math.floor(Number(prompt('Enter you Number: ')));
 console.log('Random Number - ' + a);
 console.log('You Number - ' + b)
-console.log('Result - ' + (a==b));
+console.log('Result - ' + (a===b));
